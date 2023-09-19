496d4ed5fb5a9a64af2930bffcbae8e0683b1b92
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2023-09-19 16:26:28 +0530

    Food Ordering App Splash screen (1)

--- app/src/main/java/com/example/foodorderingapp/SplashScreen.kt
+++ app/src/main/java/com/example/foodorderingapp/SplashScreen.kt
@@ -1,11 +1,20 @@
 package com.example.foodorderingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
+@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
     }
 }
